99c7a011ad84d78f2d9770e4d9150af06fc6e423
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-08-24 22:51:41 +0545

    Build Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
+import DropdownComponent from "./components/DropdownComponent";
+import { MdSwapVert } from "react-icons/md";
 
 
 function App() {
-  return(
-  <h1 className="text-3xl font-bold underline bg-sky-400 text-center text-white">
-      Hello world!
-    </h1>
-    )
+  return (
+    <>
+      <div
+        className="fontFamily-poppins w-full h-screen bg-cover bg-center bg-fixed"
+        style={{ backgroundImage: `url("night.jpg")`, opacity: 0.7 }}
+      >
+        <div className="flex justify-center justify-items-center items-center h-full">
+
+          <div className="max-w-xl bg-[#0207287f] p-5 border rounded-xl shadow-md shadow-gray-500">
+
+            <h1 className="text-xl text-center font-semibold text-white">
+              Currency Converter
+            </h1>
+
+            <div className="flex flex-col gap-3">
+
+              <div>
+                <h3 className="text-lg text-white font-semibold tracking-wider py-2 ">From:</h3>
+
+                <DropdownComponent />
+
+              </div>
+
+              <button className="bg-white rounded-md max-w-8 p-1 border-white border shadow-md shadow-white opacity-65 flex justify-self-center mt-3 mx-auto">
+
+                <MdSwapVert className="text-2xl" />
+
+              </button>
+               
+              <div>
+
+                <h3 className="text-lg text-white font-semibold tracking-wider py-2">To:</h3>
+
+                <DropdownComponent />
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/DropdownComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//  https://api.frankfurter.app/currencies
+//  https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
+
+function DropdownComponent() {
+    return (
+        <div className="max-w-xl bg-[#e0302383] p-2 border rounded-md shadow-md shadow-teal-600">
+                <h1 className="text-xl font-semibold text-white">
+                    
+                </h1>
+            </div>
+        
+    )
+}
+
+export default DropdownComponent
