1c02343e39e3d67760e68513ec65eddcf03e1103
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-12 12:01:15 +0545

    Add Dropdown component comments

--- src/components/DropdownComponent.jsx
+++ src/components/DropdownComponent.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import { IoStarOutline, IoStar } from "react-icons/io5";
 
-//  https://api.frankfurter.app/currencies
-//  https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
-
 const DropdownComponent = ({
-    currencies,
-    currency,
-    setCurrency,
-    favourites,
-    handleFavourite,
-    title = "",
+    currencies, // List of all available currencies
+    currency, // Selected currency value
+    setCurrency, // Function to update selected currency
+    favourites, // List of user's favorite currencies
+    handleFavourite, // Function to toggle a currency as a favorite
+    title = "", // Label for the dropdown
 }) => {
+    // Helper function to check if the current currency is a favorite
     const isFavorite = (curr) => favourites.includes(curr);
 
     return (
         <div>
+            {/* Dropdown label */}
             <label htmlFor="{title}" className='text-white font-semibold'>{title}</label>
+            
+            {/* Dropdown container */}
             <div className='relative'>
+                {/* Currency selection dropdown */}
                 <select
                     value={currency}
-                    onChange={(e) => setCurrency(e.target.value)}
+                    onChange={(e) => setCurrency(e.target.value)} // Update selected currency
                     className='w-full p-2 rounded-md mt-2 bg-[#514ef586] text-white font-medium shadow-md shadow-indigo-200' >
+                    
+                    {/* Display favorite currencies first */}
+                    
                     {favourites.map((currency) => {
                         return (
                             <option className="bg-yellow-600" value={currency} key={currency}>
@@ -30,6 +35,7 @@ const DropdownComponent = ({
                         );
                     })}
                     <hr />
+                    {/* Display all other available currencies */}
                     {currencies?.map((currency) => {
                         return (
                             <option value={currency} key={currency}>{currency}</option>
@@ -38,12 +44,13 @@ const DropdownComponent = ({
 
                 </select>
 
+                {/* Button to toggle favorite status of selected currency */}
                 <button
                     onClick={() => handleFavourite(currency)}
-                    className='absolute text-white font-extrabold inset-y-0 right-0 pr-7 pt-1 text-lg'>
+                    className='absolute text-white font-extrabold inset-y-0 right-0 mr-7 pt-1 text-lg'>
+                    
+                    {/* Show star icon based on favorite status */}
                     {isFavorite(currency) ? <IoStar className='text-yellow-400'/> : <IoStarOutline />}
-
-
                 </button>
 
             </div>
@@ -51,3 +58,5 @@ const DropdownComponent = ({
     )
 }
 export default DropdownComponent
+
+
